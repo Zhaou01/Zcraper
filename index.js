59d9ccd21a6e91fef2ea3380cfe19700f1855a0a
bpbot33 <brunopereyra.dev@gmail.com>
2022-12-29 01:36:19 -0300

    Scrapper worlking populing db correctly

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 const PORT = 8000;
 const express = require("express");
 const app = express();
-const Character = require("./models/character.model");
+
 const db = require("./db");
-// const popuplateDb = require("./populateDb");
+//const popuplateDb = require("./populateDb");
 
 app.get("/", (req, res) => {
   res.send("hello world");
-});
-
-app.get("/all", (req, res) => {
-  Character.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
--- populateDb.js
+++ populateDb.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { find } = require("./models/character.model");
 
 const Character = require("./models/character.model");
 
@@ -7,7 +8,6 @@ const url = "https://one-piece.com/log/character.html?p=";
 const onePiece = "https://one-piece.com";
 
 const articles = [];
-const charArr = [];
 
 let char = {
   name: "",
@@ -38,15 +38,14 @@ dataForScrap().finally(() => {
     const ir = "link";
     const filtro = articles.filter((link) => link !== ir);
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < filtro.length; i++) {
       await axios(filtro[i].link).then((response) => {
         const html = response.data;
         const $ = cheerio.load(html);
-        $(".thumbBox > p ", html).each(
-          function () {
-            const avatar = $(this).find("img").attr("src");
-            char.avatar = onePiece + avatar;
-          },
+        $(".thumbBox > p ", html).each(function () {
+          const avatar = $(this).find("img").attr("src");
+          char.avatar = onePiece + avatar;
+        }),
           $(".contDerail > p ", html).each(function () {
             const imagen = $(this).find("img").attr("src");
             const charName = filtro[i].link
@@ -55,19 +54,30 @@ dataForScrap().finally(() => {
             const charImg = onePiece + imagen;
             char.name = charName;
             char.img = charImg;
-            $(".thumbBox > p ", html).each(function () {
-              const avatar = $(this).find("img").attr("src");
-              char.avatar = onePiece + avatar;
-            }, charArr.push(char));
-            console.log(char);
-            const character = new Character(char);
+          });
+      });
+      console.log(char);
 
+      Character.find(
+        { avatar: char.avatar },
+        { name: 0, img: 0 },
+        function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Second function call: ", docs);
+          }
+          if (docs.length < 1) {
+            console.log("se guarda");
+            const character = new Character(char);
             character.save(function (err, doc) {
               console.log(doc.id);
             });
-          })
-        );
-      });
+          } else {
+            console.log("no se guarda nada");
+          }
+        }
+      );
     }
   };
   imgscrap();
